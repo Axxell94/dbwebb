9e0837344c9e464cdf60c38fdee1599d5d09ac64
Axxell94 <144466357+Axxell94@users.noreply.github.com>
2023-10-09 13:51:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,43 +14,44 @@ def main():
             print("Bye, bye - and welcome back anytime!")
             stop = True
 
-         elif choice == "13":
-            hej = input("Search a country: ")
-            search_country(hej)        
-            if ValueError:  
+        elif choice == "13":
+            try:
+                hej = input("Search a country: ")
+                emission_functions.search_country(hej)        
+            except ValueError:  
                 print("Country does not exist!")
-
-         elif choice == "13":
-            hej = input("Search a country: ")
-            emission_functions.search_country(hej)        
-            #if ValueError:  
-               # print("Country does not exist!")
-               # ska fånga exception som jag raise i functionen, hur?
             
-
-         elif choice == "14":
+        elif choice == "14":
             hej = input(" Search for a country and 1-2 years (1990, 2005, 2017) ")
-            hej = hej.split(" ")
+            hej = hej.split(",")
         
             if len(hej) == 3:
                 get =emission_functions.get_country_change_for_years(hej[0], hej[1], hej[2])
                 print(get)
             elif len(hej) == 2:
                 get=emission_functions.get_country_year_data_megaton(hej[0], hej[1])
-                print(get)  
-            elif len(hej) == 4: #Hong Kong och sen även United States of America sabbar de övre
-                hej= ((hej[0]+ " " + hej[1]), hej[2], hej[3])
-                get =emission_functions.get_country_change_for_years(hej[0], hej[1], hej[2])
-                print(get)    
-            elif len(hej) > 4:
+                print(get)      
+            elif len(hej) >= 4:
                 print("Too many inputs")
             else:    
                 print("Too few inputs")
-            # ska fånga exception som jag raise
+                
+            except 
 
         elif choice == "15":
             country_name = input("Enter a country: ")
             data= emission_functions.get_country_data(country_name) 
             emission_functions.print_country_data(data)
-    
-main()
+
+        
+
+
+        else:
+            print("That is not a valid choice. You can only choose from the menu.")
+
+        if not stop:
+            input("\nPress enter to continue...")
+
+
+if __name__ == "__main__":
+    main()
